57b8801a6886ade70dba275fcc21b818108721b0
kristenmoran <kristen.moran30@gmail.com>
2020-05-18 18:54:39 -0400

    updated bootstrap styling

--- pathfinderstore/src/components/CategoryPage/CategoryPage.js
+++ pathfinderstore/src/components/CategoryPage/CategoryPage.js
@@ -1,29 +1,31 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
+import Container from 'react-bootstrap/Container';
 
 function CategoryPage() {
 	return (
 		<div>
 			<Header />
 			<h2>Category Name Goes Here</h2>
-			<div class='row'>
-				<div class='col-sm-6'>
-					<div class='card'>
-						<div class='card-body'>
-							<h5 class='card-title'>Item Name</h5>
-							<p class='card-text'>
-								With supporting text below as a natural lead-in to additional
-								content.
-							</p>
-							<Link to={`/item`} className='item btn stretched-link'>
-								Item
-							</Link>
+			<Container className='container-fluid d-flex justify-content-center'>
+				<div class='row'>
+					<div class='col-sm-6'>
+						<div class='card'>
+							<div class='card-body'>
+								<h5 class='card-title'>Item Name</h5>
+								<p class='card-text'>
+									With supporting text below as a natural lead-in to additional
+									content.
+								</p>
+								<Link to={`/item`} className='item btn stretched-link'>
+									Item
+								</Link>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</Container>
 		</div>
 	);
 }
