c9ca5fa94ccda16eae18873ec81d4e0bbdcd57e6
kristenmoran <kristen.moran30@gmail.com>
2020-05-18 14:22:52 -0400

    put in bootstrap code fro category page

--- pathfinderstore/src/components/CategoryPage/CategoryPage.js
+++ pathfinderstore/src/components/CategoryPage/CategoryPage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 function CategoryPage() {
-	return <div>Hello from Item page</div>;
+	return (
+		<div class='row'>
+			<div class='col-sm-6'>
+				<div class='card'>
+					<div class='card-body'>
+						<h5 class='card-title'>Special title treatment</h5>
+						<p class='card-text'>
+							With supporting text below as a natural lead-in to additional
+							content.
+						</p>
+						<Link to={`/item`} className='item btn stretched-link'>
+							Item
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default CategoryPage;
