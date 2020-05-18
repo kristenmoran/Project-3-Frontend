d796195e99beed2e7369e564f1e9337c24d3980a
kristenmoran <kristen.moran30@gmail.com>
2020-05-18 19:26:46 -0400

    added styling

--- pathfinderstore/src/components/CategoryPage/CategoryPage.js
+++ pathfinderstore/src/components/CategoryPage/CategoryPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function CategoryPage() {
 	return (
@@ -9,8 +11,8 @@ function CategoryPage() {
 			<Header />
 			<h2>Category Name Goes Here</h2>
 			<Container className='container-fluid d-flex justify-content-center'>
-				<div class='row'>
-					<div class='col-sm-6'>
+				<Row class='row'>
+					<Col class='col-sm-12'>
 						<div class='card'>
 							<div class='card-body'>
 								<h5 class='card-title'>Item Name</h5>
@@ -23,8 +25,40 @@ function CategoryPage() {
 								</Link>
 							</div>
 						</div>
-					</div>
-				</div>
+					</Col>
+				</Row>
+				<Row class='row'>
+					<Col class='col-sm-12'>
+						<div class='card'>
+							<div class='card-body'>
+								<h5 class='card-title'>Item Name</h5>
+								<p class='card-text'>
+									With supporting text below as a natural lead-in to additional
+									content.
+								</p>
+								<Link to={`/item`} className='item btn stretched-link'>
+									Item
+								</Link>
+							</div>
+						</div>
+					</Col>
+				</Row>
+				<Row class='row'>
+					<Col class='col-sm-12'>
+						<div class='card'>
+							<div class='card-body'>
+								<h5 class='card-title'>Item Name</h5>
+								<p class='card-text'>
+									With supporting text below as a natural lead-in to additional
+									content.
+								</p>
+								<Link to={`/item`} className='item btn stretched-link'>
+									Item
+								</Link>
+							</div>
+						</div>
+					</Col>
+				</Row>
 			</Container>
 		</div>
 	);
