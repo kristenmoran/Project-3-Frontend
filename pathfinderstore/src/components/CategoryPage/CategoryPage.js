e4247df7ef19b9251d9e53be478bf496d97247f4
kristenmoran <kristen.moran30@gmail.com>
2020-05-18 23:43:31 -0400

    more styling added to Category and Item Pages

--- pathfinderstore/src/components/CategoryPage/CategoryPage.js
+++ pathfinderstore/src/components/CategoryPage/CategoryPage.js
@@ -4,16 +4,18 @@ import Header from '../Header/Header';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import './CategoryPage.css';
 
 function CategoryPage() {
 	return (
-		<div>
+		<div className='categoryPage'>
 			<Header />
-			<h2>Category Name Goes Here</h2>
+			<h2 className='categoryTitle'>Category Name Goes Here</h2>
 			<Container className='container-fluid d-flex justify-content-center'>
 				<Row class='row'>
-					<Col class='col-sm-12'>
-						<div class='card'>
+					<Col sm={true} className='mb-3 col-lg-8 col-m2-12'>
+						<Card className='categoryCard'>
 							<div class='card-body'>
 								<h5 class='card-title'>Item Name</h5>
 								<p class='card-text'>
@@ -24,12 +26,12 @@ function CategoryPage() {
 									Item
 								</Link>
 							</div>
-						</div>
+						</Card>
 					</Col>
 				</Row>
 				<Row class='row'>
-					<Col class='col-sm-12'>
-						<div class='card'>
+					<Col sm={true} className='mb-3 col-lg-8 col-m2-12'>
+						<Card className='categoryCard'>
 							<div class='card-body'>
 								<h5 class='card-title'>Item Name</h5>
 								<p class='card-text'>
@@ -40,12 +42,12 @@ function CategoryPage() {
 									Item
 								</Link>
 							</div>
-						</div>
+						</Card>
 					</Col>
 				</Row>
 				<Row class='row'>
-					<Col class='col-sm-12'>
-						<div class='card'>
+					<Col sm={true} className='mb-3 col-lg-8 col-m2-12'>
+						<Card className='categoryCard'>
 							<div class='card-body'>
 								<h5 class='card-title'>Item Name</h5>
 								<p class='card-text'>
@@ -56,7 +58,7 @@ function CategoryPage() {
 									Item
 								</Link>
 							</div>
-						</div>
+						</Card>
 					</Col>
 				</Row>
 			</Container>
--- pathfinderstore/src/components/ItemPage/Item.js
+++ pathfinderstore/src/components/ItemPage/Item.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../Header/Header';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './Item.css';
+
 function Item() {
 	return (
-		<div>
-			<Header />
-			<Row className='row justify-content-center'>
-				<Col className='col-md-6 justify-content-center'>
-					<h3 class='my-3'>Item Description</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
-						euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem,
-						adipiscing in adipiscing et, interdum nec metus. Mauris ultricies,
-						justo eu convallis placerat, felis enim.
-					</p>
-					<h3 class='my-3'>Item Details</h3>
-					<ul>
-						<li>Lorem Ipsum</li>
-						<li>Dolor Sit Amet</li>
-						<li>Consectetur</li>
-						<li>Adipiscing Elit</li>
-					</ul>
-				</Col>
-			</Row>
+		<div className='itemPage'>
+			<Container>
+				<Header />
+				<Row className='row justify-content-center'>
+					<Col className='itemDescription col-md-6 justify-content-center'>
+						<h3 className='itemTitle'>Item Description</h3>
+						<p className='itemParagraph'>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+							viverra euismod odio, gravida pellentesque urna varius vitae. Sed
+							dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris
+							ultricies, justo eu convallis placerat, felis enim.
+						</p>
+						<h3 className='listTitle'>Item Details</h3>
+						<ul className='itemList'>
+							<li>Lorem Ipsum</li>
+							<li>Dolor Sit Amet</li>
+							<li>Consectetur</li>
+							<li>Adipiscing Elit</li>
+						</ul>
+					</Col>
+				</Row>
+			</Container>
 		</div>
 	);
 }
