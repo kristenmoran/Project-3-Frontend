a4f56f655f9393daad85621359cddb9df2f905aa
kristenmoran <kristen.moran30@gmail.com>
2020-05-19 19:46:48 -0400

    able to fetch api data

--- pathfinderstore/src/App.js
+++ pathfinderstore/src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CategoryPage from './components/CategoryPage/CategoryPage';
+import AdventuringCategory from './components/AdventuringCategory/AdventuringCategory';
 import ItemPage from './components/ItemPage/Item';
 
 function App() {
+	const url = 'https://pathfinder-inventory.herokuapp.com/shop/items/';
+	const [categories, setItems] = useState([]);
+
+	useEffect(() => {
+		fetch(url)
+			.then((response) => response.json())
+			.then((response) => {
+				setItems(response);
+			})
+			.catch(console.error);
+	}, []);
+
+	if (categories.length === 0) {
+		return <div className='searching'>Gathering Items</div>;
+	}
+	console.log(categories);
+
 	return (
 		<div className='App'>
 			<Switch>
-				<Route path='/items' exact={true} component={CategoryPage} />
-				<Route path='/item' exact={true} component={ItemPage} />
+				<Route
+					path={`/items/adventuring`}
+					exact={true}
+					component={AdventuringCategory}
+				/>
+				<Route path={`/item`} exact={true} component={ItemPage} />
 			</Switch>
 		</div>
 	);
---
+++ pathfinderstore/src/components/AdventuringCategory/AdventuringCategory.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../Header/Header';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import './AdventuringCategory.css';
+
+function CategoryPage() {
+	const url = `https://pathfinder-inventory.herokuapp.com/shop/items/adventuring`;
+	const [categories, setItems] = useState([]);
+
+	useEffect(() => {
+		fetch(url)
+			.then((response) => response.json())
+			.then((response) => {
+				setItems(response);
+			})
+			.catch(console.error);
+	}, [url]);
+
+	if (categories.length === 0) {
+		return <div className='searching'>Gathering Items</div>;
+	}
+	console.log(categories);
+
+	return (
+		<div className='categoryPageContainer'>
+			<Header />
+			<h2 className='categoryTitle'>Adventuring Items</h2>
+			<Container className='container-fluid d-flex justify-content-center'>
+				<Row>
+					{categories &&
+						categories.map((category) => (
+							<Col
+								sm={true}
+								key={category.id}
+								className='col-lg-4 col-sm-12 col-md-6'>
+								<Card
+									style={{ width: '18rem' }}
+									key={category.id}
+									className='categoryCard'>
+									<div className='card-body'>
+										<h5 className='card-title'>{category.name}</h5>
+										<Link
+											to={`/item/${category.id}`}
+											className='item btn stretched-link'>
+											{category.categoryTwo}
+										</Link>
+									</div>
+								</Card>
+							</Col>
+						))}
+				</Row>
+			</Container>
+		</div>
+	);
+}
+
+export default CategoryPage;
