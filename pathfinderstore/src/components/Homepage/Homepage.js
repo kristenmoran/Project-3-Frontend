b4a3d80a8a8145697d48bcf05075914069b01d40
justinpedowitz <justin@pedowitz.net>
2020-05-19 22:30:19 -0700

    added and image to the background and styled more

--- pathfinderstore/src/components/Homepage/Homepage.js
+++ pathfinderstore/src/components/Homepage/Homepage.js
@@ -5,6 +5,7 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import Card from 'react-bootstrap/Card';
 import './Homepage.css';
+import Container from 'react-bootstrap/Container';
 
 function MyModal(props) {
 	return (
@@ -50,104 +51,106 @@ function Homepage() {
 	const [modalShow, setModalShow] = React.useState(false);
 
 	return (
-		<div>
-			<Button
-				className='modal-button'
-				variant='primary'
-				onClick={() => setModalShow(true)}>
-				About!
-			</Button>
-			<MyModal show={modalShow} onHide={() => setModalShow(false)} />
-			<Carousel
-				additionalTransfrom={0}
-				arrows
-				autoPlaySpeed={3000}
-				centerMode={true}
-				className=''
-				containerClass='container'
-				dotListClass=''
-				draggable
-				focusOnSelect={false}
-				infinite={false}
-				itemClass=''
-				keyBoardControl
-				minimumTouchDrag={80}
-				renderButtonGroupOutside={false}
-				renderDotsOutside={false}
-				responsive={{
-					desktop: {
-						breakpoint: {
-							max: 3000,
-							min: 1024,
+		<Container className='container-fluid d-flex justify-content-center'>
+			<div className='homepage'>
+				<Button
+					className='modal-button'
+					variant='primary'
+					onClick={() => setModalShow(true)}>
+					About this App
+				</Button>
+				<MyModal show={modalShow} onHide={() => setModalShow(false)} />
+				<Carousel
+					additionalTransfrom={0}
+					arrows
+					autoPlaySpeed={3000}
+					centerMode={true}
+					className='carousel'
+					containerClass='container'
+					dotListClass=''
+					draggable
+					focusOnSelect={false}
+					infinite={false}
+					itemClass=''
+					keyBoardControl
+					minimumTouchDrag={80}
+					renderButtonGroupOutside={false}
+					renderDotsOutside={false}
+					responsive={{
+						desktop: {
+							breakpoint: {
+								max: 3000,
+								min: 1024,
+							},
+							items: 3,
+							partialVisibilityGutter: 40,
 						},
-						items: 3,
-						partialVisibilityGutter: 40,
-					},
-					mobile: {
-						breakpoint: {
-							max: 464,
-							min: 0,
+						mobile: {
+							breakpoint: {
+								max: 464,
+								min: 0,
+							},
+							items: 1,
+							partialVisibilityGutter: 30,
 						},
-						items: 1,
-						partialVisibilityGutter: 30,
-					},
-					tablet: {
-						breakpoint: {
-							max: 1024,
-							min: 464,
+						tablet: {
+							breakpoint: {
+								max: 1024,
+								min: 464,
+							},
+							items: 2,
+							partialVisibilityGutter: 30,
 						},
-						items: 2,
-						partialVisibilityGutter: 30,
-					},
-				}}
-				showDots={false}
-				sliderClass=''
-				slidesToSlide={1}
-				swipeable>
-				<Card style={{ width: '18rem' }}>
-					<Card.Title>Weapon</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-				</Card>
-				<Card style={{ width: '18rem' }}>
-					<Card.Title>Armor</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-				</Card>
-				<Card style={{ width: '18rem' }}>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-				</Card>
-				<Card style={{ width: '18rem' }}>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-				</Card>
-				<Card style={{ width: '18rem' }}>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-				</Card>
-				<Card style={{ width: '18rem' }}>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-				</Card>
-			</Carousel>
-		</div>
+					}}
+					showDots={false}
+					sliderClass=''
+					slidesToSlide={1}
+					swipeable>
+					<Card style={{ width: '18rem' }}>
+						<Card.Title>Weapon</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the
+							bulk of the card's content.
+						</Card.Text>
+					</Card>
+					<Card style={{ width: '18rem' }}>
+						<Card.Title>Armor</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the
+							bulk of the card's content.
+						</Card.Text>
+					</Card>
+					<Card style={{ width: '18rem' }}>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the
+							bulk of the card's content.
+						</Card.Text>
+					</Card>
+					<Card style={{ width: '18rem' }}>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the
+							bulk of the card's content.
+						</Card.Text>
+					</Card>
+					<Card style={{ width: '18rem' }}>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the
+							bulk of the card's content.
+						</Card.Text>
+					</Card>
+					<Card style={{ width: '18rem' }}>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the
+							bulk of the card's content.
+						</Card.Text>
+					</Card>
+				</Carousel>
+			</div>
+		</Container>
 	);
 }
 
