0e90bb6c132037e80941893dfe4fd6d2a6f94ab7
kristenmoran <kristen.moran30@gmail.com>
2020-05-19 23:33:43 -0400

    all routes added

--- pathfinderstore/src/App.js
+++ pathfinderstore/src/App.js
@@ -3,6 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AdventuringCategory from './components/AdventuringCategory/AdventuringCategory';
 import AlchemicalCategory from './components/AlchemicalCategory/AlchemicalCategory';
+import FoodDrinkCategory from './components/FoodDrinkCategory/FoodDrinkCategory';
+import ArmorShieldCategory from './components/ArmorShieldCategory/ArmorShieldCategory';
+import ClothingCategory from './components/ClothingCategory/ClothingCategory';
+import ContainersCategory from './components/ContainersCategory/ContainersCategory';
+import WeaponsCategory from './components/WeaponsCategory/WeaponsCategory';
+import WritingCategory from './components/WeaponsCategory/WeaponsCategory';
 import ItemPage from './components/ItemPage/Item';
 
 function App() {
@@ -27,15 +33,33 @@ function App() {
 		<div className='App'>
 			<Switch>
 				<Route
-					path={`/items/adventuring`}
+					path={`/adventuring`}
 					exact={true}
 					component={AdventuringCategory}
 				/>
 				<Route
-					path={`/items/alchemical`}
+					path={`/alchemical`}
 					exact={true}
 					component={AlchemicalCategory}
 				/>
+				<Route
+					path={`/food_drink`}
+					exact={true}
+					component={FoodDrinkCategory}
+				/>
+				<Route
+					path={`/armor_shield`}
+					exact={true}
+					component={ArmorShieldCategory}
+				/>
+				<Route path={`/weapons`} exact={true} component={WeaponsCategory} />
+				<Route path={`/clothing`} exact={true} component={ClothingCategory} />
+				<Route
+					path={`/containers`}
+					exact={true}
+					component={ContainersCategory}
+				/>
+				<Route path={`/writing`} exact={true} component={WritingCategory} />
 				<Route path={`/item`} exact={true} component={ItemPage} />
 			</Switch>
 		</div>
