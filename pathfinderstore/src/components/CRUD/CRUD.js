5cdf79784f7ef7d01f04304f358e563bc3a18c34
kristenmoran <kristen.moran30@gmail.com>
2020-05-20 22:44:43 -0400

    added a bootstrap field for crud

--- pathfinderstore/src/components/CRUD/CRUD.js
+++ pathfinderstore/src/components/CRUD/CRUD.js
@@ -1,41 +1,60 @@
-
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
 import './CRUD.css';
 
 function CRUD() {
-    const url = `https://pathfinder-inventory.herokuapp.com/shop/items`;
-    const [ editItem , setEditItem] = useState([]);
-    useEffect(() => {
-			fetch(url)
-				.then((response) => response.json())
-				.then((response) => {
-					setItems(response);
-				})
-				.catch(console.error);
-        }, [url]);
-        function handleSubmit(event) {
-            event.preventDefault();
-            
-        }
+	const url = `https://pathfinder-inventory.herokuapp.com/shop/items`;
+	const [editItem, setEditItem] = useState([]);
+	const [setString] = useState([]);
+
+	useEffect(() => {
+		fetch(url)
+			.then((response) => response.json())
+			.then((response) => {
+				setEditItem(response);
+			})
+			.catch(console.error);
+	}, [url]);
+
+	function handleChange(event) {
+		setString(event.target.value);
+	}
+
+	function createItem(item) {
+		return fetch(url, {
+			method: 'POST',
+			body: JSON.stringify(item),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((res) => {
+				return res;
+			})
+			.catch((err) => err);
+	}
+
 	return (
 		<div className='testing'>
 			<h1>Edit An Item</h1>
-			<form action='/recipes' method='post' acceptCharset='utf-8'>
-				<label className='title'>Name:</label>
-				<input type='text' id='name' name='name' />
-
-				<label className='url'>Description:</label>
-				<input type='text' id='url' name='url' />
-
-				<label className='ingredients'>Category:</label>
-				<input type='text' id='ingredients' name='ingredients' />
-
-				<button type='submit'>Add Item</button>
-				<button type='submit'>Update Item</button>
-				<button type='submit'>Delete Item</button>
-			</form>
+			<Form>
+				<Form.Group controlId='exampleForm.ControlInput1'>
+					<Form.Label>Name</Form.Label>
+					<Form.Control type='text' placeholder='ex. Bag of Holding' />
+				</Form.Group>
+				<Form.Group controlId='exampleForm.ControlInput1'>
+					<Form.Label>Category</Form.Label>
+					<Form.Control type='text' placeholder='ex. Adventuring' />
+				</Form.Group>
+				<Form.Group controlId='exampleForm.ControlTextarea1'>
+					<Form.Label>Item Description</Form.Label>
+					<Form.Control as='textarea' rows='3' />
+				</Form.Group>
+			</Form>
+			<button type='submit'>Add Item</button>
+			<button type='submit'>Update Item</button>
+			<button type='submit'>Delete Item</button>
 		</div>
 	);
 }
-
 export default CRUD;
